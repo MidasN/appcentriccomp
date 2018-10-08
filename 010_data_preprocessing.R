2885ea98387483c2237e7720a0a9849351b73eb9
Stalkcomrade <stanislavpoznyakof@gmail.com>
2018-10-08 09:12:13 +0200

    updates state of code

---
+++ 010_data_preprocessing.R
@@ -0,0 +1,90 @@
+
+library(tidyverse)
+library(haven)
+library(stringr)
+library(reshape)
+library(magrittr)
+
+
+df <- read_spss("./data/KnowledgeWorkerSurvey_originaldata.sav")
+
+datamap <- list()
+datamap$DISCO <- c("Chief executives, senior officials, and legislators" = 1,
+                   "Administrative and commercial managers" = 2,
+                   "Production and specialised services managers" = 3,
+                   "Hospitality, retail and other services managers" = 4,
+                   "Science and engineering professionals" = 5,
+                   "Health professionals" = 6,
+                   "Teaching professionals" = 7,
+                   "Business and administration professionals" = 8,
+                   "ICT professionals" = 9,
+                   "Legal, social, and cultural professionals" = 10,
+                   "Science and engineering, shipping and aviation associate professionals" = 11,
+                   "Health associate professionals" = 12,
+                   "Business, economics, and administration associate professionals" = 13,
+                   "Legal, social, and cultural associated professionals" = 14,
+                   "ICT technicians" = 15)
+
+
+# Forget about consolidated for now
+
+datamap$DISCO_Consolidated <- c("Chief executives, senior officials, and legislators" = 1,
+                                "General management" = 2,
+                                "General management" = 3,
+                                "Hospitality and retail" = 4,
+                                "Science and engineering" = 5,
+                                "Health" = 6,
+                                "Teaching" = 7,
+                                "Business and administration" = 8,
+                                "ICT" = 9,
+                                "Legal, social, and cultural" = 10,
+                                "Science and engineering" = 11,
+                                "Health" = 12,
+                                "Business, economics, and administration" = 13,
+                                "Legal, social, and cultural" = 14,
+                                "ICT" = 15)
+
+
+
+# Only included completed surveys
+
+
+df_clean = df %>%
+  filter(status == 1) %>% # Filter to only included completed surveys
+  select(-(sc1_4:sc1_11_43)) %>% # Delete screen-out occupation columns
+  mutate(Occupation_Num = rowSums(select(., sc1_1:sc1_3), na.rm = T), # Merge columns of occupations
+         Occupation_DISCO = names(datamap$DISCO[Occupation_Num]), # English labels for DISCO 2008 Occupations
+         Occupation = names(datamap$DISCO_Consolidated[Occupation_Num])) %>% # Merged sector labels for Occupations
+  select(-(sc1_1:sc1_3)) %>% # Remove unnecessary occupation columns
+  mutate_at(vars(Q3_1:Q3_4), funs(.-1)) # Q3_1:Q3_4, 5 means 5+
+
+
+# Precleaning Using OpenRefine
+
+
+## Cleaned Using OpenRefine, see changefile at DataCleaningSoftwareNames_OpenRefine.gz
+software_cleaned <- read_tsv("./data/softwarenames_cleaned.tsv") %>%
+  separate_rows(a, sep = ";") %>%
+  mutate(a = str_trim(a)) %>%  # Remove all year versions from MS
+  mutate(a2 = str_replace(a, " [:digit:][:digit:][:digit:][:digit:]$", "")) %>% # Remove all year versions from MS
+  mutate(a2 = str_replace(a2, "SAP.*", "SAP")) %>% # Remove NA
+  filter(!str_detect(a2, "NA"))
+
+software_cleaned$RecordNo %<>% as.numeric()
+
+
+# Aggregated Numbers
+
+
+software_aggregated <- software_cleaned %>% # Group and order high to low
+  group_by(a2) %>% count(a2) %>% arrange(desc(n)) %>% # Show cumulative relative frequency of apps
+  ungroup() %>%
+  mutate(relcumsum = cumsum(n / 4131 * 100))
+
+
+
+jobtitleanddescription <- df_clean[, c("RecordNo", "Q18_1_open", "Q18a", "Occupation_DISCO", "Occupation")]
+merged <- merge(x = software_cleaned, y = jobtitleanddescription,
+                by = "RecordNo") %>%
+  select(-a, -Q18_1_open, -Q18a, -Occupation_DISCO)
+
--- functions.R
+++ functions.R
@@ -1,3 +1,97 @@
+library(magrittr)
+
+#' based on input calculate distance matrix;
+#' apply k-means given number of clustersf
+#' @param pre_dist_input is the dataset.
+#' @param method is method for distance calculation.
+#' @param labels_col is number of labels to plot per cluster
+#' @param seed is number for kmeans replication
+#' @param column_exclude_start is used for filtering columns NOT to include
+#' @param column_exclude_end is used for filtering columns NOT to include
+#' @param additional_exlude is convenient to exclude several variables
+#' @return plot.
+
+flexible_clustering <- function(pre_dist_input, method, clust_n, labels_col, column_exclude_start, column_exclude_end, additional_exclude = NULL, seed) {
+
+  set.seed(seed)
+
+  pre_dist = pre_dist_input[,-c(1, column_exclude_start:column_exclude_end)]
+
+  if (!is.null(additional_exclude)){
+
+    pre_dist = pre_dist %>% dplyr::select(-additional_exclude)
+
+  } else {
+
+    print("Nothing to exlude")
+}
+
+  d = dist(pre_dist[,-1],
+           method)
+
+  fit = cmdscale(d, k = 2)
+  fit_tb = fit %>% as_tibble()
+  colnames(fit_tb) = c("Dim.1", "Dim.2")
+
+
+  x = fit[, 1]
+  y = fit[, 2]
+
+  kmeans_output = kmeans(fit_tb, clust_n)
+
+  clust = kmeans_output$cluster
+  clust_centers = kmeans_output$centers[,-3] %>% as.data.frame()
+  colnames(clust_centers)[1:2] = c("Dim.1.c", "Dim.2.c")
+
+  clust_centers %<>%
+    mutate(groups = rownames(clust_centers) %>% as.character())
+
+  # clust_centers
+
+  fit_tb = fit_tb %>% # making it global for later use in for_labels df
+    mutate(groups = clust %>% as.character(),
+           index = 1:nrow(fit_tb))
+
+  fit_tb = inner_join(fit_tb, clust_centers,
+                      by = "groups")
+
+  # combining 6 labels per group which based on euclidian dist
+
+  fit_tb %<>%
+    group_by(groups) %>%
+    mutate(dist = sqrt((Dim.1 - Dim.1.c)^2 + (Dim.2 - Dim.2.c)^2),
+           abs_dist = abs(Dim.1 - Dim.1.c) + (Dim.2 - Dim.2.c))
+
+  index_labels_balanced = c(
+    fit_tb %>%
+      group_by(groups) %>%
+      arrange(dist, .by_group = TRUE) %>%
+      filter(row_number() %in% (1:labels_col)) %$% index)
+
+
+  index_labels_balanced = index_labels_balanced[!duplicated(index_labels_balanced)]
+
+  plot = ggscatter(fit_tb %>% ungroup() %>% arrange(index),
+          x = "Dim.1", y = "Dim.2",
+          label = pre_dist_input$a,
+          label.select = pre_dist_input$a[index_labels_balanced],
+          color = "groups",
+          # star.plot = TRUE,
+          palette = "jco",
+          size = 1,
+          ellipse = TRUE,
+          ellipse.type = "convex",
+          repel = TRUE) +
+    geom_point(data = clust_centers, aes(x = Dim.1.c, y = Dim.2.c, size = 13))
+
+
+  returnList = list("plot" = plot,
+                      "labels" = pre_dist_input$a, "selectedLables" = pre_dist_input$a[index_labels_balanced],
+                      "fit" = fit_tb %>% ungroup() %>% arrange(index))
+
+  return(returnList)
+}
+
 
 
 # the main purpose is to got clusters labels before plotting
@@ -45,39 +139,6 @@ plot_mds <- function(pre_dist, method, labels_to_select, clust_n) {
             repel = TRUE)
 }
 
-
-mds_n_plot <- function(pre_dist, method, labels_to_select, clust_n){
-
-  d = dist(pre_dist[,-1], method = method)
-  fit = cmdscale(d, k = 2)
-
-  fit_tb = fit %>% as_tibble()
-  colnames(fit_tb) = c("Dim.1", "Dim.2")
-
-
-  x = fit[, 1]
-  y = fit[, 2]
-
-  clust <<- kmeans(fit_tb, clust_n)$cluster %>% as.factor() ## FIXME: delete global declaration
-  fit_tb = fit_tb %>% # making it global for later use in for_labels df
-    mutate(groups = clust)
-
-
-  ggscatter(fit_tb,
-          x = "Dim.1", y = "Dim.2",
-          label = pre_dist$a,
-          label.select = labels_to_select,
-          color = "groups",
-      #    star.plot = TRUE,
-          palette = "jco",
-          size = 1,
-          ellipse = TRUE,
-          ellipse.type = "convex",
-          repel = TRUE)
-#  + geom_hline(yintercept = 0, color = "red", size = 2)
-
-
-}
 
 explore_model_fit <- function(df, n_profiles_range = 1:6, model_names = c("EII", "VVI", "EEE", "VVV")) {
     x = mclustBIC(df, G = n_profiles_range, modelNames = model_names)
